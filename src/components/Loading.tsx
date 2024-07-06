9dcc4aaffe0471ac05fb219e457cc8848677c882
hashnj <joshinikhilesh46@gmail.com>
2024-07-06 19:52:48 +0530

    loading update

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,6 @@
 export const Loading=()=>{
     return(
-        <div className="w-screen h-screen flex justify-center items-center bg-yellow-800 text-yellow-200 ">
+        <div className="w-screen h-screen flex justify-center items-center bg-yellow-800 text-yellow-200 text-4xl">
             Loading...
         </div>
     )
